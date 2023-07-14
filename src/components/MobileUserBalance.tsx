aa942a74de02657a641b1f39f57a563236b9650c
Jinho Kim <jinojoa317@naver.com>
2023-07-15 01:51:33 +0900

    🔥FEAT: Finish User Mobile Payment Page

--- src/components/MobileUserBalance.tsx
+++ src/components/MobileUserBalance.tsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import { wallet, walletBalance, allowButton } from "../styles/mobileUserBalance";
+import {
+  wallet,
+  walletBalance,
+  allowButton,
+} from "../styles/mobileUserBalance";
 
-const MobileUserBalance = () => {
+const MobileUserBalance = (props: any) => {
+  const [isSecurePurchase, setIsSecurePurchase] = useState(false);
+  const [userInfo, setUserInfo] = useState({} as any);
+
+  useEffect(() => {
+    fetchData(); // Fetch data from the backend when the component mounts
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8800/api/user/username/${props.user.username}`
+      ); // Make a GET request to the backend API
+      setUserInfo(response.data.user); // Set the state with the response data
+      setIsSecurePurchase(response.data.user.isSecurePurchase);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const onClickAllow = () => {
+    if (isSecurePurchase === true) {
+      axios
+        .post(
+          `http://localhost:8800/api/user/purchase/${props.user.username}`,
+          {}
+        )
+        .then((res) => {
+          if (res.status === 200) {
+            alert("1분간 결제가 허용됩니다!");
+            window.location.reload();
+          } else {
+            alert("결제 승인에 실패했습니다.");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      alert("안전결제 기능이 비활성화 되어있습니다.");
+    }
+  };
+
   return (
     <div style={wallet}>
-      <span style={walletBalance}>10000원</span>
-      <button style={allowButton}>결제 승인</button>
+      <span style={walletBalance}>{userInfo.balance} 원</span>
+      <button style={allowButton} onClick={onClickAllow}>
+        결제 승인
+      </button>
     </div>
   );
 };
--- src/pages/MobileUserMainPage.tsx
+++ src/pages/MobileUserMainPage.tsx
@@ -1,36 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 import MobileNavMain from "../components/MobileNavMain";
 import MobileUserPayment from "../components/MobileUserPayment";
 import MobileUserBalance from "../components/MobileUserBalance";
 
 const MobileUserMainPage = () => {
+  const [data, setData] = useState([] as any);
   const token = localStorage.getItem("token") as string;
 
   let decoded: any;
   try {
     decoded = jwt_decode(token);
-  } catch (err) {} 
+  } catch (err) {}
 
-  
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8800/api/purchase/user/${decoded.login.username}`
+      );
+      setData(response.data.purchases);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  console.log(data);
+  data.map((payment: any) => {
+    // leave only time
+    payment.time = payment.time
+    console.log(payment.time.split("T")[0])
+  })
+  // map mobile user payment component
+  let paymentList = data.map((payment: any) => (
+    <MobileUserPayment
+      price={payment.price}
+      balance={payment.userAmount}
+      time={payment.time}
+      store={payment.nameOfStore}
+      product={payment.product}
+    />
+  ));
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <MobileNavMain name={decoded.login.username} />
-      <MobileUserBalance />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
-      <MobileUserPayment price="1000" balance="20000" time="10시 45분 10초" store="대취타" product="랜덤박스" />
+      <MobileUserBalance user={decoded.login} />
+      {paymentList}
     </>
   );
 };
